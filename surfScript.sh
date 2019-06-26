2397d8ecdc53e64de51a304636f586152c72ca99
jgfruitbomb <jgfruitbomb@gmail.com>
2019-06-26 14:24:32 -0500

    Fixed problem when the wave size was not -

--- surfScript.sh
+++ surfScript.sh
@@ -3,15 +3,9 @@
 curl https://magicseaweed.com/The-Cross-Pensacola-Beach-Surf-Report/955/ > surf.html
 
 
-SIZE=$(grep -o -P "<li class=\"rating-text text-dark\">.{0,7}" surf.html | tail -c 4)
+SIZE=$(grep -o -P "<li class=\"rating-text text-dark\">.{0,7}" surf.html | tail -c 4 | sed 's/<s//g')
 DAY=$(date +"%m/%d")
-BIG=$(echo "$SIZE" | tail -c 2)
 
 echo -e "Today is $DAY"
-if [ $BIG -gt 2 ]
-then
-	echo -e "The surf is $SIZE ft my dude\n Might be a good time to hit the gnar"
-else
-	echo "The surf is $SIZE ft my dude"
-fi
+echo "The surf is $SIZE ft my dude"
 
