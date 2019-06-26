956c369e81591b1730f9a547bdd50d75b259a6ba
jgfruitbomb <jgfruitbomb@gmail.com>
2019-06-27 00:46:20 +0100

    Added comments and variables for other people to input their information. Also added logic for when it is 'flat'

--- surfScript.sh
+++ surfScript.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+#This link is specifically for pensacola
 curl https://magicseaweed.com/The-Cross-Pensacola-Beach-Surf-Report/955/ > surf.html
 
 
 SIZE=$(grep -o -P "<li class=\"rating-text text-dark\">.{0,7}" surf.html | tail -c 4 | sed 's/<s//g')
 DAY=$(date +"%m/%d")
+PHONE={Insert Your Phone Number}
 
-echo -e "Today is $DAY"
-echo "The surf is $SIZE ft my dude"
+#The value for this variable can be found here https://www.cid.ninja
+PROVIDER={Insert Your Service Provider Email}
+
+if [ $SIZE == "fla" ]
+then
+	echo -e "Today is $DAY \nThe surf is 0 ft my dude" | mail -s "" $PHONE@$PROVIDER
+
+else
+	echo -e "Today is $DAY \nThe surf is $SIZE ft my dude" | mail -s "" $PHONE@$PROVIDER
+fi
+
 
